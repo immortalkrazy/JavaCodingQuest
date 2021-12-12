e1e3a9e50672920aaf979422353f40095708d251
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2021-12-12 00:39:01 -0500

    added UniqueElement class

---
+++ JavaCodingQuest/src/codes/UniqueElement.java
@@ -0,0 +1,39 @@
+package codes;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniqueElement {
+
+      public static void main(String[] args) {
+
+            List<Integer> list = new ArrayList<Integer>();
+            try (Scanner sc = new Scanner(System.in)) {
+                  System.out.println("please numbers");
+                  for (int i = 0; i < 5; i++) {
+                        list.add(sc.nextInt());
+                  }
+            }
+            System.out.println(list.size());
+            System.out.println("Unique element is: " + uniqueElement(list));
+
+      }
+
+      public static int uniqueElement(List<Integer> list) {
+
+            Set<Integer> set = new HashSet<>();
+
+            for (int i = 0; i < list.size(); i++) {
+                  if (!set.add(list.get(i))) {
+                        set.remove(list.get(i));
+                  }
+
+            }
+            List<Integer> lst = new ArrayList<>(set);
+            return lst.get(0);
+      }
+
+}
