b766f7ae3e73b1b3d206eb0f98582dfb174087c3
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2021-12-12 22:49:57 -0500

    added JsonParser class

--- JavaCodingQuest/src/codes/JsonParse.java
+++ JavaCodingQuest/src/codes/JsonParse.java
@@ -1,10 +1,12 @@
 package codes;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -19,14 +21,40 @@ public class JsonParse {
 
       public static List<String> getArticle(int max) throws IOException, ParseException {
 
+            String fileName = "sample.json";
+            String directory = System.getProperty("user.dir");
+            String path = "";
+            path = directory + File.separator + fileName;
+            System.out.println(path);
+
+            File file = new File(path);
+            try {
+                  if (file.createNewFile()) {
+                        System.out.println("New File Created");
+                  } else {
+                        System.out.println("File Already Exists");
+                  }
+            } catch (Exception e) {
+                  e.printStackTrace();
+            }
+
             List<String> list = new ArrayList<>();
             JSONParser jp = new JSONParser();
-            FileReader reader = new FileReader("test.json");
-            Object obj = jp.parse(reader);
-            JSONObject articleJO = (JSONObject) obj;
+            FileReader reader = new FileReader(path);
+            JSONObject jo = (JSONObject) jp.parse(reader);
 
-            String title = (String) articleJO.get("title");
-            System.out.println("Title: " + title);
+            JSONArray ja = (JSONArray) jo.get("data");
+
+            List<Object> jList = new ArrayList<>(ja);
+            for (Object obj : jList) {
+                  System.out.println(obj);
+            }
+
+//            Object obj = jp.parse(reader);
+//            JSONObject articleJO = (JSONObject) obj;
+//
+//            String title = (String) articleJO.get("data[1]");
+//            System.out.println("Title: " + title);
 
             return list;
       }
