60ef58cdfdf19eda0558c7b72f8263b6f8b33b0e
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2022-01-07 00:34:19 -0500

    some correction

--- JavaCodingQuest/src/codes/TestRuns.java
+++ JavaCodingQuest/src/codes/TestRuns.java
@@ -7,6 +7,8 @@ public class TestRuns {
             final String opening = "[{(";
             final String closing = "]})";
 
+            String s = "123";
+
 //            System.out.println(opening.indexOf('['));
 //            System.out.println(opening.indexOf('{'));
 //            System.out.println(opening.indexOf('('));
@@ -18,13 +20,14 @@ public class TestRuns {
             String str3 = "abc";
             String str = "abcdef";
 
-            System.out.println(str1.length());
-            System.out.println(str2.length());
-            System.out.println(str3.length());
-            System.out.println(str.substring(1).contentEquals("bcdef"));
-            System.out.println(str.contains("e"));
-            System.out.println(str.substring(1, 5));
+//            System.out.println(str1.length());
+//            System.out.println(str2.length());
+//            System.out.println(str3.length());
+//            System.out.println(str.substring(1).contentEquals("bcdef"));
+//            System.out.println(str.contains("e"));
+//            System.out.println(str.substring(1, 5));
 
+            System.out.println(Integer.parseInt(s));
       }
 
 }
