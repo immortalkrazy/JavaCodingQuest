7276e462c04188539a0ae8e92e00392f63614072
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2022-01-16 22:44:11 -0500

    momoization technique added in Fibonacci class for better time complexity.

--- JavaCodingQuest/src/codes/Fibonacci.java
+++ JavaCodingQuest/src/codes/Fibonacci.java
@@ -2,19 +2,45 @@ package codes;
 
 public class Fibonacci {
 
+      private static long[] fibonacciCache;
+
       public static void main(String[] args) {
 
-            int num = 12;
+            int num = 50;
+
+            long start1 = System.currentTimeMillis();
             System.out.println(fib(num));
+            long end1 = System.currentTimeMillis();
+            System.out.println((end1 - start1) / 1000);
+
+            fibonacciCache = new long[num + 1];
+            long start2 = System.currentTimeMillis();
+            System.out.println(fibMemo(num));
+            long end2 = System.currentTimeMillis();
+            System.out.println((end2 - start2) / 1000);
       }
 
-      public static int fib(int num) {
-            if (num == 0)
-                  return 0;
-            else if (num == 1)
-                  return 1;
-            else {
-                  return fib(num - 1) + fib(num - 2);
+      public static long fib(int num) {
+
+            if (num <= 1)
+                  return num;
+
+            return fib(num - 1) + fib(num - 2);
+      }
+
+      public static long fibMemo(int n) {
+
+            if (n <= 1) {
+                  return n;
             }
+
+            if (fibonacciCache[n] != 0) {
+                  return fibonacciCache[n];
+            }
+
+            long nthFibonacciNumber = (fibMemo(n - 1) + fibMemo(n - 2));
+            fibonacciCache[n] = nthFibonacciNumber;
+
+            return nthFibonacciNumber;
       }
 }
